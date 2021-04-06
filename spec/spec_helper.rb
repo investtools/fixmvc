e8aed864e0e904ec2e9e053f1d86db3f4a0e5489
Fabiano Martins <fabiano@investtools.com.br>
2021-04-06 17:53:51 -0300

    Fixing Wrong Constants

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,8 @@
-if ENV.include?('CODECLIMATE_REPO_TOKEN')
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
-
-require 'ftpmvc/test_helpers'
+require 'fixmvc/test_helpers'
 require 'active_support/dependencies'
 
 RSpec.configure do |config|
-  config.include FTPMVC::TestHelpers
+  config.include FIXMVC::TestHelpers
   config.before :each do
     ActiveSupport::Dependencies.clear
   end
